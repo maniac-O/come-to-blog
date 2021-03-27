7e382400fc98c4595531f6aa346e84186535f619
cj45645@naver.com <50076751+maniac-O@users.noreply.github.com>
2021-03-28 03:37:39 +0900

    made dynamic grid's order attribute

--- Blog/dist/newspaperJS.dev.js
+++ Blog/dist/newspaperJS.dev.js
@@ -4,39 +4,87 @@ $(document).ready(function () {
   var contents = document.querySelector('.contents');
   var data_index = document.querySelectorAll('[data-index]');
   var buttons = document.querySelectorAll('wrapnav > ul > li');
+  var jqIndex = $('[data-index]');
+  var jqI_len = jqIndex.length;
   var p_tag = '<span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex possimus ipsum, beatae unde doloribus repellat odio tempora omnis earum quo? Doloribus incidunt labore at commodi veritatis. Accusamus nihil eaque modi?</span>';
+  var oldArray = [];
+  var newArray = [];
   var contentscnt = createContents();
   insertContents();
+  insertOrder();
   statbar();
+
+  function insertOrder() {
+    var nowIndex = $('[data-index]');
+    var cnt = 0;
+
+    for (var i = 0; i < jqI_len; i++) {
+      oldArray[i] = oldArray[i].index;
+      $(nowIndex[i]).css('order', oldArray[i]);
+    } //이거 진짜 노력의 산물인데 개멍청했네 시발;
+
+
+    for (var _i = 0; _i < nowIndex.length; _i++) {
+      //console.log(i+1,newArray[cnt]);
+      if (_i + 1 == newArray[cnt]) {
+        //console.log('inside i =',i,$(nowIndex[jqI_len+cnt]).data(), newArray[cnt]);
+        $(nowIndex[jqI_len + cnt]).css('order', newArray[cnt]);
+        cnt++;
+      } // 한 행마다 3번째 gird는 폰트스타일 변경
+
+
+      if (Math.floor($(nowIndex[_i]).data('index') % 5) == 3) {
+        //console.log(i+1);
+        $(nowIndex[_i]).css('font-family', "'Syne Mono', 'monospace'", 'font-weight', 'bold', 'font-size', '2rem');
+      }
+    }
+  }
 
   function createContents() {
     var div_length = data_index.length;
+    var oldCnt = 0;
+    var newCnt = 0;
+    var cnt = 0;
 
-    for (var i = div_length + 1; i < div_length + 10; i++) {
-      contents.innerHTML += '<div data-index="' + i + '"></div>';
+    for (var i = 0; i < jqI_len; i++) {
+      oldArray[i] = jqIndex.eq(i).data();
     }
 
-    return div_length + 9;
+    for (var _i2 = 1; _i2 < div_length + 11; _i2++) {
+      // 아직 생성되지 않은 곳에 order속성 추가시키기
+      // 이미 생성된 data-index는 넘김
+      var alreadyMade = data_index[oldCnt].dataset.index;
+
+      if (alreadyMade == _i2) {
+        oldCnt++;
+        newCnt++;
+        if (oldCnt == div_length) oldCnt--;
+        continue;
+      } // 생성되지 않은 data-index는 div를 생성
+
+
+      contents.innerHTML += '<div data-index="' + _i2 + '"></div>';
+      newCnt++;
+      newArray[cnt] = newCnt;
+      cnt++;
+    }
+
+    return div_length + 10;
   }
 
   function insertContents() {
-    var contents_div = document.querySelectorAll('[data-index]');
+    contents_div = document.querySelectorAll('[data-index]');
 
-    for (var i = 0; i < contents_div.length; i++) {
+    for (var i = 0; i < contentscnt; i++) {
+      // 본문채우기
       for (var j = 0; j < 4; j++) {
         if (i == 1 && j == 2) {
-          contents_div[i].innerHTML += '<div><img src="./pic/HEOS01BW.png" alt=""></img></div>';
+          contents_div[i].innerHTML += '<span>RESTORANCE IS EVERYWHERE.. MAYBE IN YOUR COMPUTER</span><div><img src="./pic/HEOS01BW.png" alt=""></img></div>';
           contents_div[i].innerHTML += '<div>' + p_tag + '</div>';
           continue;
         }
 
         contents_div[i].innerHTML += p_tag;
-      }
-
-      if (Math.floor(i % 5) == 2) {
-        contents_div[i].style.fontFamily = 'Syne Mono', 'monospace';
-        contents_div[i].style.fontWeight = 'bold';
-        contents_div[i].style.fontSize = '2rem';
       }
     }
   }
--- Blog/newspaperJS.js
+++ Blog/newspaperJS.js
@@ -2,41 +2,98 @@ $(document).ready(function(){
     let contents= document.querySelector('.contents');
     let data_index = document.querySelectorAll('[data-index]');
     let buttons = document.querySelectorAll('wrapnav > ul > li');
+    let jqIndex = $('[data-index]');
+    let jqI_len =jqIndex.length;
     let p_tag = '<span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex possimus ipsum, beatae unde doloribus repellat odio tempora omnis earum quo? Doloribus incidunt labore at commodi veritatis. Accusamus nihil eaque modi?</span>';
-    
+    let oldArray = [];
+    let newArray = [];
     
 
-    const contentscnt = createContents()
+    const contentscnt = createContents();
     insertContents();
+    insertOrder();
     statbar();
+
+
+    function insertOrder(){
+        let nowIndex = $('[data-index]');
+        let cnt = 0;
+        
+        
+        for(let i=0; i<jqI_len; i++){
+            oldArray[i] = oldArray[i].index;
+            $(nowIndex[i]).css('order',oldArray[i]);
+        }
+        
+        //이거 진짜 노력의 산물인데 개멍청했네 시발;
+        for(let i=0; i<nowIndex.length; i++){
+            //console.log(i+1,newArray[cnt]);
+            if(i+1 == newArray[cnt]){
+                //console.log('inside i =',i,$(nowIndex[jqI_len+cnt]).data(), newArray[cnt]);
+                $(nowIndex[jqI_len+cnt]).css('order',newArray[cnt]);
+                cnt++;
+            }
+
+            // 한 행마다 3번째 gird는 폰트스타일 변경
+            if(Math.floor( $(nowIndex[i]).data('index')%5) == 3){
+                //console.log(i+1);
+                $(nowIndex[i]).css('font-family',"'Syne Mono', 'monospace'", 
+                                    'font-weight','bold',
+                                    'font-size','2rem');
+            }
+        }
+        
+    }
 
     function createContents(){
         let div_length = data_index.length;
+        let oldCnt=0;
+        let newCnt=0;
+        let cnt=0;
         
-        for(let i=div_length+1; i<(div_length+10); i++){
-            contents.innerHTML += '<div data-index="' + i +  '"></div>';
+        for(let i=0; i<jqI_len; i++){
+            oldArray[i] = jqIndex.eq(i).data();
         }
-        return div_length+9;
+        
+
+        for(let i=1; i<(div_length+11); i++){
+            // 아직 생성되지 않은 곳에 order속성 추가시키기
+            
+            
+            // 이미 생성된 data-index는 넘김
+            let alreadyMade = data_index[oldCnt].dataset.index;
+            if(alreadyMade == i){
+                oldCnt++;
+                newCnt++;
+
+                if(oldCnt==div_length)
+                    oldCnt--;
+                continue;
+            }
+            // 생성되지 않은 data-index는 div를 생성
+            contents.innerHTML += '<div data-index="' + i +  '"></div>';
+
+            newCnt++;
+            newArray[cnt] = newCnt;
+            cnt++;
+            
+        }
+        return div_length+10;
     }
 
     function insertContents(){
-        let contents_div = document.querySelectorAll('[data-index]');
-
-        for(let i=0; i<contents_div.length; i++){
+        contents_div = document.querySelectorAll('[data-index]');
+        for(let i=0; i<contentscnt; i++){
+            // 본문채우기
             for(let j=0; j<4; j++){
+                
                 if(i==1 && j==2){
-                    contents_div[i].innerHTML +='<div><img src="./pic/HEOS01BW.png" alt=""></img></div>'
+                    contents_div[i].innerHTML +='<span>RESTORANCE IS EVERYWHERE.. MAYBE IN YOUR COMPUTER</span><div><img src="./pic/HEOS01BW.png" alt=""></img></div>'
                     contents_div[i].innerHTML +=('<div>'+ p_tag + '</div>');
                     continue;
                 }
                 
                 contents_div[i].innerHTML += p_tag;
-            }
-            if(Math.floor(i%5) == 2){
-                
-                contents_div[i].style.fontFamily = 'Syne Mono', 'monospace';
-                contents_div[i].style.fontWeight = 'bold';
-                contents_div[i].style.fontSize = '2rem';
             }
         }
     }
