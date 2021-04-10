233c41f237f248763cc8120db07d85af69ff1260
maniac-O <cj45645@naver.com>
2021-04-10 19:05:55 +0900

    First ResponsiveWeb

---
+++ ResponsiveWeb/MyResponsiveWeb01.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.menu-button').click(function(){
+        $('.header-nav').toggleClass('active');
+    });
+});
---
+++ ResponsiveWeb/dist/MyResponsiveWeb01.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.menu-button').click(function () {
+    $('.header-nav').toggleClass('active');
+  });
+});
