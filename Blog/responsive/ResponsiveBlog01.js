4e7017d9f77b9f1a630852fb173d07b3be2d6a3a
cj45645@naver.com <50076751+maniac-O@users.noreply.github.com>
2021-04-16 23:37:02 +0900

    Test: Test Array

--- Blog/responsive/ResponsiveBlog01.js
+++ Blog/responsive/ResponsiveBlog01.js
@@ -1,1 +1,8 @@
 // Object, 객체지향, method
+$(window).ready(()=>{
+    let fruits = [1,2,3,4,5]
+    
+    fruits.splice(1,2,2,3);
+    console.log(fruits);
+});
+
--- Blog/responsive/dist/ResponsiveBlog01.dev.js
+++ Blog/responsive/dist/ResponsiveBlog01.dev.js
@@ -1,2 +1,8 @@
-// Object, 객체지향, method
 "use strict";
+
+// Object, 객체지향, method
+$(window).ready(function () {
+  var fruits = [1, 2, 3, 4, 5];
+  fruits.splice(1, 2, 2, 3);
+  console.log(fruits);
+});
