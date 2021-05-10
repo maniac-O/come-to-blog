77fa9bb84925fc0619b494d9bf2681b0e91ec417
cj45645@naver.com <50076751+maniac-O@users.noreply.github.com>
2021-05-11 00:09:40 +0900

    Add: NodeJS Function Modi Del

--- DailyCoding/NodeJS/dist/main.dev.js
+++ DailyCoding/NodeJS/dist/main.dev.js
@@ -2,52 +2,137 @@
 
 var http = require('http');
 
-var fs = require('fs'); //let url = require('url');
+var fs = require('fs');
 
+var qs = require('querystring');
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = new URL('http://127.0.0.1:8080' + _url);
   var pathname = queryData.pathname;
   var title = queryData.searchParams.get('id');
+  var template = '';
+
+  function rendering(title, list, body, control) {
+    template = "<!doctype html>\n        <html>\n        <head>\n            <title>Node.JS - ".concat(title, "</title>\n            <meta charset=\"utf-8\">\n        </head>\n        <body>\n        <h1><a href=\"/\">WEB</a></h1>\n            ").concat(list, "\n            ").concat(control, "\n            ").concat(body, "\n        </body>\n        </html>\n        ");
+  }
+
+  function rendList(filelist) {
+    this.filelist = filelist;
+    var list = "<ul>";
+
+    for (var i = 0; i < this.filelist.length; i++) {
+      list += "<li><a href=\"/?id=".concat(this.filelist[i], "\">").concat(this.filelist[i], "</a></li>");
+    }
+
+    list += "</ul>";
+    return list;
+  }
 
   if (pathname === '/') {
     fs.readdir('./data', function (err, filelist) {
       if (err) console.log('err!!');
-      var template = '';
       var description = ''; // ul태그 안 list를 동적으로 만드는 동작
 
-      var list = "<ul>";
-
-      for (var i = 0; i < filelist.length; i++) {
-        list += "<li><a href=\"/?id=".concat(filelist[i], "\">").concat(filelist[i], "</a></li>");
-      }
-
-      list += "</ul>";
-
-      function rendering(title, list, body) {
-        template = "<!doctype html>\n                <html>\n                <head>\n                  <title>Node.JS - ".concat(title, "</title>\n                  <meta charset=\"utf-8\">\n                </head>\n                <body>\n                  <h1><a href=\"/\">WEB</a></h1>\n                  ").concat(list, "\n                  ").concat(body, "\n                </body>\n                </html>\n              ");
-        response.writeHead(200);
-        response.end(template);
-      } // ul태그 아래 title과 본문내용 변경
-
+      var list = rendList(filelist); // ul태그 아래 title과 본문내용 변경
 
       if (title == undefined) {
         title = 'Welcome';
         description = 'Welcome!!!';
-        rendering(title, list, "<h2>".concat(title, "</h2>").concat(description));
+        rendering(title, list, "<h2>".concat(title, "</h2>").concat(description), "<a href=\"/create\">create</a>");
+        response.writeHead(200);
+        response.end(template);
       } else if (title) {
         fs.readFile("./data/".concat(title), 'utf-8', function (err, data) {
           description = data;
-          rendering(title, list, "<h2>".concat(title, "</h2>").concat(description));
+          rendering(title, list, "<h2>".concat(title, "</h2>").concat(description), "<a href=\"/create\">create</a> \n                        <a href=\"/update?id=".concat(title, "\">update</a>\n                        <form action=\"/delete_process\" method=\"post\" onsubmit=\"checkToSubmit\">\n                            <input type=\"hidden\" name=\"id\" value=\"").concat(title, "\">\n                            <input type=\"submit\" value=\"delete\">\n                        </form>"));
+          response.writeHead(200);
+          response.end(template);
         });
       }
+    });
+  } else if (pathname === '/create') {
+    fs.readdir('./data', function (err, filelist) {
+      if (err) console.log('err!!');
+      title = 'NODE.JS - create';
+      var list = rendList(filelist);
+      var description = "<form action=\"/create_process\" method=\"post\">\n                <p><input type=\"text\" name=\"title\" placeholder=\"title\"></p>\n                <p><textarea name=\"description\" id=\"\" cols=\"30\" rows=\"10\" placeholder=\"description\"></textarea></p>\n                <p><input type=\"submit\"></p>\n            </form>";
+      rendering(title, list, description, "<a href=\"/create\">create</a>");
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathname === '/create_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body += data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      title = post.title;
+      var description = post.description;
+      fs.writeFile("data/".concat(title), description, 'utf8', function (err) {
+        if (err) throw err;
+        response.writeHead(302, {
+          Location: "/?id=".concat(title)
+        });
+        response.end();
+      });
+    });
+  } else if (pathname === '/update') {
+    var list;
+    fs.readdir('./data', function (err, filelist) {
+      list = rendList(filelist);
+    });
+    fs.readFile("./data/".concat(title), 'utf-8', function (err, data) {
+      description = data;
+      rendering(title, list, "<form action=\"/update_process\" method=\"post\">\n                    <input type=\"hidden\" name=\"id\" value=\"".concat(title, "\">\n                    <p><input type=\"text\" name=\"title\" placeholder=\"title\" value=\"").concat(title, "\"></p>\n                    <p><textarea name=\"description\" id=\"\" cols=\"30\" rows=\"10\" placeholder=\"description\">").concat(description, "</textarea></p>\n                    <p><input type=\"submit\"></p>\n                </form>"), "<a href=\"/create\">create</a> <a href=\"/update?id=".concat(title, "\">update</a>"));
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathname === '/update_process') {
+    var _body = '';
+    request.on('data', function (data) {
+      _body += data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(_body);
+      var id = post.id;
+      title = post.title;
+      var description = post.description; // 파일명 바꾸기
+
+      fs.rename("data/".concat(id), "data/".concat(title), function (err) {
+        if (err) console.log('rename error'); //파일 내용 바꾸기
+
+        fs.writeFile("data/".concat(title), description, 'utf8', function (err) {
+          if (err) throw err;
+          response.writeHead(302, {
+            Location: "/?id=".concat(title)
+          });
+          response.end();
+        });
+      });
+    });
+  } else if (pathname === '/delete_process') {
+    var _body2 = '';
+    request.on('data', function (data) {
+      _body2 += data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(_body2);
+      var id = post.id; // 파일 삭제 기능
+
+      fs.unlink("data/".concat(id), function (err) {
+        response.writeHead(302, {
+          Location: "/"
+        });
+        response.end();
+      });
     });
   } else {
     response.writeHead(404);
     response.end('Not found');
   }
 });
-app.listen(8080); // cd E:\web\come-to-blog\DailyCoding\NodeJS
+app.listen(8080); // cd E:\web\come-to-blog\DailyCoding\NodeJSS
 // nodemon main.js
 // pm2 start .\main.js
--- DailyCoding/NodeJS/main.js
+++ DailyCoding/NodeJS/main.js
@@ -1,55 +1,177 @@
-let http = require('http');
-let fs = require('fs');
-//let url = require('url');
+const http = require('http');
+const fs = require('fs');
+const qs = require('querystring');
 
-let app = http.createServer(function (request, response) {
+const app = http.createServer(function (request, response) {
     let _url = request.url;
     let queryData = new URL('http://127.0.0.1:8080' + _url);
     let pathname = queryData.pathname;
     let title = queryData.searchParams.get('id');
+    let template = '';
+
+    function rendering(title, list, body, control) {
+        template = `<!doctype html>
+        <html>
+        <head>
+            <title>Node.JS - ${title}</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+            ${list}
+            ${control}
+            ${body}
+        </body>
+        </html>
+        `;
+    }
+
+    function rendList(filelist) {
+        this.filelist = filelist;
+        let list = `<ul>`;
+        for (let i = 0; i < this.filelist.length; i++) {
+            list += `<li><a href="/?id=${this.filelist[i]}">${this.filelist[i]}</a></li>`;
+        }
+        list += `</ul>`;
+        return list;
+    }
 
     if (pathname === '/') {
         fs.readdir('./data', (err, filelist) => {
             if (err) console.log('err!!');
-            let template = '';
+
             let description = '';
 
             // ul태그 안 list를 동적으로 만드는 동작
-            let list = `<ul>`;
-            for (let i = 0; i < filelist.length; i++) {
-                list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            }
-            list += `</ul>`;
-
-            function rendering(title, list, body) {
-                template = `<!doctype html>
-                <html>
-                <head>
-                  <title>Node.JS - ${title}</title>
-                  <meta charset="utf-8">
-                </head>
-                <body>
-                  <h1><a href="/">WEB</a></h1>
-                  ${list}
-                  ${body}
-                </body>
-                </html>
-              `;
-                response.writeHead(200);
-                response.end(template);
-            }
+            let list = rendList(filelist);
 
             // ul태그 아래 title과 본문내용 변경
             if (title == undefined) {
                 title = 'Welcome';
                 description = 'Welcome!!!';
-                rendering(title, list, `<h2>${title}</h2>${description}`);
+                rendering(
+                    title,
+                    list,
+                    `<h2>${title}</h2>${description}`,
+                    `<a href="/create">create</a>`
+                );
+                response.writeHead(200);
+                response.end(template);
             } else if (title) {
                 fs.readFile(`./data/${title}`, 'utf-8', (err, data) => {
                     description = data;
-                    rendering(title, list, `<h2>${title}</h2>${description}`);
+                    rendering(
+                        title,
+                        list,
+                        `<h2>${title}</h2>${description}`,
+                        `<a href="/create">create</a> 
+                        <a href="/update?id=${title}">update</a>
+                        <form action="/delete_process" method="post" onsubmit="checkToSubmit">
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                        </form>`
+                    );
+                    response.writeHead(200);
+                    response.end(template);
                 });
             }
+        });
+    } else if (pathname === '/create') {
+        fs.readdir('./data', (err, filelist) => {
+            if (err) console.log('err!!');
+
+            title = 'NODE.JS - create';
+            let list = rendList(filelist);
+            let description = `<form action="/create_process" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="description" id="" cols="30" rows="10" placeholder="description"></textarea></p>
+                <p><input type="submit"></p>
+            </form>`;
+
+            rendering(title, list, description, `<a href="/create">create</a>`);
+
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else if (pathname === '/create_process') {
+        let body = '';
+        request.on('data', (data) => {
+            body += data;
+        });
+        request.on('end', () => {
+            let post = qs.parse(body);
+
+            title = post.title;
+            let description = post.description;
+
+            fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
+                if (err) throw err;
+
+                response.writeHead(302, { Location: `/?id=${title}` });
+                response.end();
+            });
+        });
+    } else if (pathname === '/update') {
+        let list;
+        fs.readdir('./data', (err, filelist) => {
+            list = rendList(filelist);
+        });
+
+        fs.readFile(`./data/${title}`, 'utf-8', (err, data) => {
+            description = data;
+            rendering(
+                title,
+                list,
+                `<form action="/update_process" method="post">
+                    <input type="hidden" name="id" value="${title}">
+                    <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                    <p><textarea name="description" id="" cols="30" rows="10" placeholder="description">${description}</textarea></p>
+                    <p><input type="submit"></p>
+                </form>`,
+                `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            );
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else if (pathname === '/update_process') {
+        let body = '';
+        request.on('data', (data) => {
+            body += data;
+        });
+        request.on('end', () => {
+            let post = qs.parse(body);
+
+            let id = post.id;
+            title = post.title;
+            let description = post.description;
+
+            // 파일명 바꾸기
+            fs.rename(`data/${id}`, `data/${title}`, (err) => {
+                if (err) console.log('rename error');
+
+                //파일 내용 바꾸기
+                fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
+                    if (err) throw err;
+
+                    response.writeHead(302, { Location: `/?id=${title}` });
+                    response.end();
+                });
+            });
+        });
+    } else if (pathname === '/delete_process') {
+        let body = '';
+        request.on('data', (data) => {
+            body += data;
+        });
+        request.on('end', () => {
+            let post = qs.parse(body);
+            let id = post.id;
+
+            // 파일 삭제 기능
+            fs.unlink(`data/${id}`, (err) => {
+                response.writeHead(302, { Location: `/` });
+                response.end();
+            });
         });
     } else {
         response.writeHead(404);
@@ -58,6 +180,6 @@ let app = http.createServer(function (request, response) {
 });
 app.listen(8080);
 
-// cd E:\web\come-to-blog\DailyCoding\NodeJS
+// cd E:\web\come-to-blog\DailyCoding\NodeJSS
 // nodemon main.js
 // pm2 start .\main.js
