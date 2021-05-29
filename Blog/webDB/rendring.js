3c937debf5d0847f6ac120509912800d721dae85
cj45645@naver.com <50076751+maniac-O@users.noreply.github.com>
2021-05-29 23:27:59 +0900

    Add: Basic Randering

--- Blog/webDB/rendring.js
+++ Blog/webDB/rendring.js
@@ -1,3 +1,17 @@
 $(document).ready(() => {
     // AJAX 구현
 });
+const http = require('http');
+const fs = require('fs');
+const qs = require('querystring');
+const path = require('path');
+const sanitizeHtml = require('sanitize-html');
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'madang',
+    password: 'madang',
+    database: 'madang',
+    port: '3306',
+});
+db.connect();
